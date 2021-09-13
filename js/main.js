d249d090014e12e019a3289fd36536d6cf0996a1
JuliusMatadamas <julius3877@gmail.com>
2021-09-13 17:25:35 -0500

    Se agrega las secciones 'Acerca' y 'Habilidades'.

--- js/main.js
+++ js/main.js
@@ -1,22 +1,22 @@
 /*===== MENU SHOW & HIDDEN =====*/
 const navMenu = document.querySelector("#nav-menu"),
-        toggleMenu = document.querySelector("#nav-toggle"),
-        closeMenu = document.querySelector("#nav-close");
+    toggleMenu = document.querySelector("#nav-toggle"),
+    closeMenu = document.querySelector("#nav-close");
 
 // SHOW MENU
-toggleMenu.addEventListener('click', ()=>{
+toggleMenu.addEventListener('click', () => {
     navMenu.classList.toggle('show');
 });
 
 // HIDE MENU
-closeMenu.addEventListener('click', ()=>{
+closeMenu.addEventListener('click', () => {
     navMenu.classList.remove('show');
 });
 
 // HIDE MENU ON CLICK IN A LINK
 const navLink = document.querySelectorAll('.nav__link');
 
-function linkAction(){
+function linkAction() {
     navMenu.classList.remove('show');
 }
 navLink.forEach(n => n.addEventListener('click', linkAction));
@@ -26,7 +26,7 @@ const sections = document.querySelectorAll('section[id]');
 
 window.addEventListener('scroll', scrollActive);
 
-function scrollActive(){
+function scrollActive() {
     const scrollY = window.pageYOffset;
 
     sections.forEach(current => {
@@ -34,11 +34,10 @@ function scrollActive(){
         const sectionTop = current.offsetTop - 50;
         sectionId = current.getAttribute('id');
 
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav__menu a[href*='+ sectionId +']').classList.add('active');
-        }
-        else {
-            document.querySelector('.nav__menu a[href*='+ sectionId +']').classList.remove('active');
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active');
+        } else {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active');
         }
     });
 }
